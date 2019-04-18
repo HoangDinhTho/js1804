7b247b9694ed8fb5da61f386ace03bb4beddf401
Huong <huongnguyenak96@gmail.com>
2019-04-18 20:33:58 +0700

    init

---
+++ a.js
@@ -0,0 +1,39 @@
+var b = 'string';
+let a = 234;
+a = 1;
+// console.log(a);
+
+const pi = 3.14;
+// pi = 1;  //false
+
+/**
+ * string
+ * number
+ * boolean
+ * array
+ * object
+ * null
+ * any
+ * undefined
+ */
+
+let s = 'asaaa';
+let o = {};
+let c; // any
+let n = '';
+let m = undefined;
+
+/**
+ * 
+ * undefined
+ * null
+ * ''
+ * false
+ * 0
+ * NaN
+ * 
+ */
+if(NaN){
+    console.log('Đúng');
+}
+else console.log('Sai');
