14e489550d7faffa0754e1a3679ae4a11160d41f
Huong <huongnguyenak96@gmail.com>
2019-04-18 20:57:28 +0700

    array

--- b.js
+++ b.js
@@ -1,1 +1,21 @@
-// console.log(b);
+// array
+let arr = [12,2,3,5,6,1];
+arr['key'] = 'value';
+arr.name = 'Teo'; 
+console.log(arr);
+console.log(typeof arr); // obj
+console.log(Array.isArray(arr)); // true
+
+// let arr02 = new Array(3,5,6);
+// for, forEach, fill, filler, map, push, pop, shift, unshift, sort, splice, reduce, findIndex
+
+
+
+
+let obj = {};
+console.log(typeof obj);
+obj['key'] = 'value';
+obj.name = 'Teo'; 
+console.log(obj);
+
+
