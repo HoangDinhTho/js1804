b641287d219ff832cc45a69c496988df64849cc6
Huong <huongnguyenak96@gmail.com>
2019-04-20 21:01:57 +0700

    d.js

--- c.js
+++ c.js
@@ -1,21 +1,51 @@
-// map, push, pop, shift, unshift, sort, splice, reduce, findIndex
+// map, push, pop, shift, unshift, sort, splice, reduce, find, findIndex
 
-let arr = ['PHP', 'Nodejs', 'iOS', 'Android'];
+// let arr = ['PHP', 'Nodejs', 'iOS', 'Android'];
+// let find = arr.find(element=>{
+//     return element=='iOS';
+// })
+// let find = arr.findIndex(element=>element=='PHP')
+// console.log(find)
+
+// let string = arr.reduce((a,b)=>{
+//     return a + ' ' + b;
+// },'React')
+// console.log(string);
+
+
+let arrNumber = [4,6,1,78,3,12];
+arrNumber.sort()
+
+// arrNumber.sort(function(a,b){});
+// arrNumber.sort((a,b)=>{
+//     // return a<=b;
+//     return a-b;
+// })
+// arrNumber.sort((a,b)=>a-b)
+// console.log(arrNumber)
+
+
+// arr.sort();
+// arr.reverse();
+// arr.sort().reverse();
+// console.log(arr);
+
+
 // arr.push('Front-end'); // add last
 // arr.unshift('Front-end'); // add first
 // arr.pop(); // remove last
 // arr.shift(); // remove first
-arr.splice(1,0,'Front end','React','HTML');// add or remove
-console.log(arr);
+// arr.splice(1,0,'Front end','React','HTML');// add or remove
+// console.log(arr);
 
 
-function a(...a){
-    // console.log(a)
-    a.map(e=>{
-        console.log(e)
-    })
-}
-a(2,4,5,6);
+// function a(...a){
+//     // console.log(a)
+//     a.map(e=>{
+//         console.log(e)
+//     })
+// }
+// a(2,4,5,6);
 
 // arr.map(element=>{
 //     console.log(element)
---
+++ d.js
@@ -0,0 +1,16 @@
+// let l = console.log
+// l(2);
+
+/**
+ * Sum number
+ * @param {number} a 
+ * @param {number} b
+ * @return number
+ */
+// let sum = function(a,b){
+//     return a+b;
+// }
+// ES6 arrow function
+let sum = (a,b)=>a+b;
+const s = sum(4,3);
+console.log(s);
