225e3e168016127509bcec23afe554d4840f83d4
Huong <huongnguyenak96@gmail.com>
2019-04-20 21:30:14 +0700

    function

--- d.js
+++ d.js
@@ -11,6 +11,42 @@
 //     return a+b;
 // }
 // ES6 arrow function
-let sum = (a,b)=>a+b;
-const s = sum(4,3);
+// let sum = (a,b)=>a+b;
+// const s = sum(4,3);
+// console.log(s);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// let s = sum(2,4);
+
+// /**
+//  * log a message
+//  * @param {function} l 
+//  */
+// function log(l){
+//     return l(1234);
+// }
+// log(console.log);
+
+function log(l){
+    return l;
+}
+// log(console.log)('Hello');
+
+function tinhtong(){
+    function sum(a,b){
+        return a+b;
+    }
+    return sum;
+}
+let s = tinhtong()(2,4);
 console.log(s);
+
+function sum(a,b){
+    return a+b;
+}
+function tinhtong02(sum){
+    return sum;
+}
+console.log(tinhtong02(sum)(2,4))
