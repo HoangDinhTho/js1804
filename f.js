7731337e7774b272615a7ecdb384d1f6408d7420
Huong <huongnguyenak96@gmail.com>
2019-04-23 21:14:26 +0700

    f.js

--- f.js
+++ f.js
@@ -39,15 +39,54 @@ function veHinh2(h=5){
             if(x<=h-y+1){
                 s += '* '
             }
+            else s +=' '
         }
         console.log(s)
     }
 }
-veHinh2();
+// veHinh2();
 
 
-//         *
-//       * *
-//     *   *
-//   *     *
-// * * * * *
+//         *    y=1; x=    5;
+//       * *    y=2; x=  4 5;
+//     *   *    y=3; x= 3  5;
+//   *     *    y=4; x=2   5;
+// * * * * *    y=5; x=12345;
+//              x=h || y=h || x= h-y+1
+function veHinh3(h=5){
+    for(let y = 1; y<=5; y++){
+        let s = '';
+        for(let x = 1; x<=5; x++){
+            if(x==h || y==h || x == h-y+1){
+                s += ' *'
+            }
+            else s +='  '
+        }
+        console.log(s)
+    }
+}
+// veHinh3();
+
+function veHinh(h=5,fn){
+    for(let y = 1; y<=5; y++){
+        let s = '';
+        for(let x = 1; x<=5; x++){
+            const dk = fn(x,y,h)
+            if(dk){
+                s += ' *'
+            }
+            else s +='  '
+        }
+        console.log(s)
+    }
+}
+// ve hinh 1
+// veHinh(5,function(x,y){
+//     return x<=y;
+// });
+//hinh 2
+// veHinh(5,function(x,y,h){
+//     return x<=h-y+1;
+// });
+//hinh3
+veHinh(5,(x,y,h)=>x==h || y==h || x == h-y+1);
